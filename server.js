b104e04830bf5f806cb797758df6fe5101181af0
SurithFernand <surith444@gmail.com>
2026-07-24 15:38:24 +0530

    Initialize route files with Express Router
    Use of express.Router() which creates a router object that lets you define routes in a modular, organized way.

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Define Routes
+// Load the ***.js(ex: users.js) route file.
+// Any request starting with '/api/***' (ex: 'api/users') goes to that file.
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
